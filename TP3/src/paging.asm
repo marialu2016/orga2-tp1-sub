000615d4611d591d0ceffb83d9b0d822daa8c392
nicog89 <nicog89@f1292de8-a922-11de-9ed3-3f1bf8abe145>
2009-11-12 22:12:56 +0000

    avances con paginacion

--- TP3/src/paging.asm
+++ TP3/src/paging.asm
@@ -1,33 +1,40 @@
+
+%define PINTOR_PAGE_DIR   0xA000
+%define TRADUC_PAGE_DIR   0xB000
+
+
+%define PINTOR_PAGE_TABLE 0xC000
+%define TRADUC_PAGE_TABLE 0xD000
+
+
 BITS 32
-mov eax, page_table_0
-or 	eax, 0x3		;supervisor, read/write, present
-mov [page_dir], eax
 
 
-page_dir:
-	dd 	0x00000000
-	
-%rep	0x400 - 1
-	dd	0x00000002		;supervisor, read/write, not present
+; Directorio de paginas del pintor (0xA000)
+dd PINTOR_PAGE_TABLE | 3
+
+%rep 0x400 - 1
+	dd 0				; 1023 paginas en blanco
 %endrep
 
-page_table_0:
+; Directorio de paginas del traductor (0xB000)
+dd TRADUC_PAGE_TABLE | 3
+
+%rep 0x400 - 1
+	dd 0				; 1023 paginas en blanco
+%endrep
+
+
+; Tabla de paginas del pintor (0xC000)
 %assign i 0x0000
 %rep    0x400
-	
-    dd 	i | 3			;supervisor, read/write, present
-				
-%assign i i+4096 
+	dd 	i | 3			;supervisor, read/write, present
+	%assign i i+4096
 %endrep
 
-
-
-
-
-
-
-
-
-
-
-
+; Tabla de paginas del traductor (0xC000)
+%assign i 0x0000
+%rep    0x400
+	dd 	i | 3			;supervisor, read/write, present
+	%assign i i+4096
+%endrep
