f9662cef691d79c95b1e1a676e7cc4ed0a50666f
nicog89 <nicog89@f1292de8-a922-11de-9ed3-3f1bf8abe145>
2009-11-12 22:54:21 +0000

    Ejercicio 2 completo.

--- trunk/TP3/src/kernel.asm
+++ trunk/TP3/src/kernel.asm
@@ -12,157 +12,158 @@ extern tsss;
 
 ;Aca arranca todo, en el primer byte.
 start:
-		cli					;no me interrumpan por ahora, estoy ocupado
-		jmp 	bienvenida
+        cli                    ;no me interrumpan por ahora, estoy ocupado
+        jmp     bienvenida
 
-;aca ponemos todos los mensajes		
+;aca ponemos todos los mensajes        
 iniciando: db 'Iniciando el kernel mas inutil del mundo'
-iniciando_len equ $ - iniciando		
+iniciando_len equ $ - iniciando        
 
 
 bienvenida:
-	IMPRIMIR_MODO_REAL iniciando, iniciando_len, 0x07, 0, 0
-	; Ejercicios AQUI
+    IMPRIMIR_MODO_REAL iniciando, iniciando_len, 0x07, 0, 0
+
+        CALL enable_A20             ; activamos A20
+        CALL check_A20
+
+        cli                         ; Deshabilitamos interrupciones
 
 
-		; TODO: Habilitar A20
-		CALL enable_A20
-		CALL check_A20
+    ; Ejercicio 1
+        
+        ; Cargamos el descriptor de la GDT
+        lgdt [GDT_DESC]
+        
+        ; Pasamos a modo protegido
+        mov     eax, cr0
+        or      eax, 01h
+        mov     cr0, eax
+        
+        jmp 0x08:modoprotegido
 
-		; TODO: Dehabilitar Interrupciones
-		cli
-
-
-	; Ejercicio 1
-		
-		; Cargamos el descriptor de la GDT
-		lgdt [GDT_DESC]
-		
-		; Pasamos a modo protegido
-		mov 	eax, cr0
-		or  	eax, 01h
-		mov 	cr0, eax
-		jmp 0x08:modoprotegido
 BITS 32
-		modoprotegido:
-                        ;Apuntamos 'es' a la memoria de video y los demas al segmento de datos
+        modoprotegido:
+            ;Apuntamos 'es' a la memoria de video y los demas al segmento de datos
+            mov ax, 0x10    ; segmento de datos
+            mov bx, 0x18    ; memoria de video
+            mov ds, ax
+            mov fs, ax
+            mov gs, ax
+            mov ss, ax
+            mov es, bx    ; es apunta a la memoria de video
+                           
+            ; Pintamos de negro la pantalla
+            mov ecx, 80*25    ;toda la pantalla
+            xor esi, esi
 
-			mov ax, 0x10	; segmento de datos
-			mov bx, 0x18	; memoria de video
-			mov ds, ax
-			mov fs, ax
-			mov gs, ax
-			mov ss, ax
-			mov es, bx	; es apunta a la memoria de video
-	                       
-                        ; Pintamos de negro la pantalla
-			mov ecx, 80*25	;toda la pantalla
-			xor esi, esi
+            mov ax, 0x0000    ; negro, ningun caracter
+            
+            cleanPantalla:
+                mov [es:esi], ax         
+                add esi, 2    ; avanza al siguiente caracter
+            loop cleanPantalla
 
-			mov ax, 0x0000	; negro, ningun caracter
-			cleanPantalla:
-				mov [es:esi], ax         
-				add esi, 2	; avanza al siguiente caracter
-				loop cleanPantalla
+            ; Dibujamos bordes horizontales
+            mov ah, 0x0F        ; blanco brillante, fondo negro
+            mov al, 0x02        ; caracter carita
+            xor esi, esi 
+            mov edi, 80*24*2
+            mov ecx, 80
+            bordeHor:
+                mov [es:esi], ax
+                mov [es:edi], ax
+                add esi, 2
+                add edi, 2
+            loop bordeHor
 
+            ; Dibujamos bordes verticales
+            xor esi, esi
+            mov edi, 79*2
+            mov ecx, 25
+            bordeVer:
+                mov [es:esi], ax
+                mov [es:edi], ax
+                add esi, 80*2
+                add edi, 80*2
+            loop bordeVer
 
-                        ; Dibujamos bordes horizontales
-			mov ah, 0x0F		; blanco brillante, fondo negro
-			mov al, 0x02		; caracter carita
-			xor esi, esi 
-			mov edi, 80*24*2
-			mov ecx, 80
-			bordeHor:
-				mov [es:esi], ax
-				mov [es:edi], ax
-				add esi, 2
-				add edi, 2
-				loop bordeHor
-			
-			xchg ebx,ebx
+            ; BREAKPOINT
+            xchg bx, bx
 
-			; Dibujamos bordes verticales
-			xor esi, esi
-			mov edi, 79*2
-			mov ecx, 25
-			bordeVer:
-				mov [es:esi], ax
-				mov [es:edi], ax
-				add esi, 80*2
-				add edi, 80*2
-				loop bordeVer
-	; Ejercicio 2
+    ; Ejercicio 2
 
-		; TODO: Habilitar paginacion
-		mov eax, 0xA000		;cargo la direccion del directorio en cr3
-		mov cr3, eax
-	
-		mov eax, cr0
-		or  eax, 0x80000000		;habilito paginacion
-		mov cr0, eax
+        mov eax, 0xA000        ;cargo la direccion del directorio en cr3
+        mov cr3, eax
+    
+        mov eax, cr0
+        or  eax, 0x80000000        ;habilito paginacion
+        mov cr0, eax
 
-		jmp $
-	
-	; Ejercicio 3
-	
-		; TODO: Inicializar la IDT
-		; TODO: Resetear la pic
+        ; BREAKPOINT
+        xchg bx, bx        
 
-		
-; 	pic_reset:
-; 		mov al, 11h
-; 		out 20h, al
+        jmp $
+
+    ; Ejercicio 3
+    
+        ; TODO: Inicializar la IDT
+        ; TODO: Resetear la pic
+
+        
+;     pic_reset:
+;         mov al, 11h
+;         out 20h, al
 ; 
-; 		mov al, 20h
-; 		out 21h, al
+;         mov al, 20h
+;         out 21h, al
 ; 
-; 		mov al, 04h
-; 		out 21h, al
+;         mov al, 04h
+;         out 21h, al
 ; 
-; 		mov al, 01h
-; 		out 21h, al
+;         mov al, 01h
+;         out 21h, al
 ; 
-; 		mov al, 0xFF
-; 		out 21h, al
+;         mov al, 0xFF
+;         out 21h, al
 ; 
-; 		mov al, 0x11
-; 		out 0xA1, al
+;         mov al, 0x11
+;         out 0xA1, al
 ; 
-; 		mov al, 0x28
-; 		out 0xA1, al
+;         mov al, 0x28
+;         out 0xA1, al
 ; 
-; 		mov al, 0x02
-; 		out 0xA1, al
+;         mov al, 0x02
+;         out 0xA1, al
 ; 
-; 		mov al, 0x01
-; 		out 0xA1, al
+;         mov al, 0x01
+;         out 0xA1, al
 ; 
-; 		mov al, 0xFF
-; 		out 0xA1, al
-; 	pic_enable:
-; 		mov al, 0x00
-; 		out 0x21, al
-; 		mov al, 0x00
-; 		out 0xA1, al
+;         mov al, 0xFF
+;         out 0xA1, al
+;     pic_enable:
+;         mov al, 0x00
+;         out 0x21, al
+;         mov al, 0x00
+;         out 0xA1, al
 ; 
-; 		; TODO: Cargar el registro IDTR
-; 		sti
-; 	jmp $
-	; Ejercicio 4
-	
-		; TODO: Inicializar las TSS
-		
-		; TODO: Inicializar correctamente los descriptores de TSS en la GDT
-		
-		; TODO: Cargar el registro TR con el descriptor de la GDT de la TSS actual
-		
-		; TODO: Habilitar la PIC
-		
-		; TODO: Habilitar Interrupciones
-		
-		; TODO: Saltar a la primer tarea
-		
-		
+;         ; TODO: Cargar el registro IDTR
+;         sti
+;     jmp $
+    ; Ejercicio 4
+    
+        ; TODO: Inicializar las TSS
+        
+        ; TODO: Inicializar correctamente los descriptores de TSS en la GDT
+        
+        ; TODO: Cargar el registro TR con el descriptor de la GDT de la TSS actual
+        
+        ; TODO: Habilitar la PIC
+        
+        ; TODO: Habilitar Interrupciones
+        
+        ; TODO: Saltar a la primer tarea
+        
+        
 %include "a20.asm"
 
 %define TASK1INIT 0x8000
--- trunk/TP3/src/paging.asm
+++ trunk/TP3/src/paging.asm
@@ -2,39 +2,114 @@
 %define PINTOR_PAGE_DIR   0xA000
 %define TRADUC_PAGE_DIR   0xB000
 
-
 %define PINTOR_PAGE_TABLE 0xC000
 %define TRADUC_PAGE_TABLE 0xD000
-
 
 BITS 32
 
 
 ; Directorio de paginas del pintor (0xA000)
-dd PINTOR_PAGE_TABLE | 3
-
-%rep 0x400 - 1
-	dd 0				; 1023 paginas en blanco
-%endrep
+    dd PINTOR_PAGE_TABLE | 3
+    
+    %rep 0x400 - 1
+        dd 0                ; 1023 paginas en blanco
+    %endrep
 
 ; Directorio de paginas del traductor (0xB000)
-dd TRADUC_PAGE_TABLE | 3
-
-%rep 0x400 - 1
-	dd 0				; 1023 paginas en blanco
-%endrep
+    dd TRADUC_PAGE_TABLE | 3
+    
+    %rep 0x400 - 1
+        dd 0                ; 1023 paginas en blanco
+    %endrep
 
 
 ; Tabla de paginas del pintor (0xC000)
-%assign i 0x0000
-%rep    0x400
-	dd 	i | 3			;supervisor, read/write, present
-	%assign i i+4096
-%endrep
+    dd 0x0000 | 3            ; kernel
+    dd 0x1000 | 3            ;
+    dd 0x2000 | 3            
+    dd 0x3000 | 3
+    dd 0x4000 | 3
+    dd 0x5000 | 3
+    dd 0x6000 | 3
+    dd 0x7000 | 3
+    
+    dd 0x8000 | 3            ; tarea del pintor
+    
+    dd 0x0000            ; 0x9000
+    dd 0x0000            ; 0xA000
+    dd 0x0000            ; 0xB000
+    dd 0x0000            ; 0xC000
+    dd 0x0000            ; 0xD000
+    
+    dd 0xE000 | 3            ; 0xE000   GDT, IDT, TSS
+    dd 0xF000 | 3        ; 0xF000   GDT, IDT, TSS
+    
+    dd 0x0000            ; 0x10000
+    dd 0x0000            ; 0x11000
+    dd 0x0000            ; 0x12000
+    
+    dd 0xb8000 | 3      ; 0x13000     (mapeado a video)
+    
+    dd 0x0000            ; 0x14000
+    
+    dd 0x15000 | 3        ; 0x15000
+    
+    %rep 0xB8 - 0x16    ; (desde 0x16 hasta 0xB8...)
+        dd 0x0000
+    %endrep
+    
+    dd 0x10000 | 3        ; 0xB8000
+    
+    %rep 0x400 - 0xB9    ; (desde 0xB9 hasta 0x400...)
+        dd 0x0000
+    %endrep
 
 ; Tabla de paginas del traductor (0xC000)
-%assign i 0x0000
-%rep    0x400
-	dd 	i | 3			;supervisor, read/write, present
-	%assign i i+4096
-%endrep
+
+    dd 0x0000 | 3            ; kernel
+    dd 0x1000 | 3            ;
+    dd 0x2000 | 3            
+    dd 0x3000 | 3
+    dd 0x4000 | 3
+    dd 0x5000 | 3
+    dd 0x6000 | 3
+    dd 0x7000 | 3
+    
+    dd 0                   ; 0x8000
+    
+    dd 0x9000 | 3            ; tarea de traductor
+    dd 0xA000 | 3            ; dir y tablas
+    dd 0xB000 | 3            ;
+    dd 0xC000 | 3            ;
+    dd 0xD000 | 3            ; /
+    dd 0xE000 | 3            ; GDT, IDT, TSS
+    dd 0xF000 | 3            ; /
+    dd 0x10000 | 3           ; Espacio de lectura del traductor
+    
+    dd 0                   ; 0x11000
+    dd 0                   ; 0x12000
+    
+    dd 0xB8000 | 3         ; 0x13000 a video
+    
+    dd 0                   ; 0x14000
+    dd 0                   ; 0x15000
+    
+    dd 0x16000 | 3         ; 0x16000    pila del traductor
+    
+    dd 0                   ; 0x17000
+    dd 0xB8000 | 3         ; 0x18000 a video
+    
+    %rep 0xA0 - 0x19       ;desde 0x19 hasta 0xA0...
+        dd 0    
+    %endrep
+    
+    %assign i 0xA0000
+    %rep 0xC0 - 0xA0       ; desde 0xA0000 a 0xC0000 identity
+        dd i | 3
+        %assign i i + 0x1000
+    %endrep
+    
+    ;Completamos con cero hasta el final!
+    %rep 0x400 - 0xC0 + 1   ; (desde 0xC1 hasta 0x400...)
+        dd 0x0000
+    %endrep
