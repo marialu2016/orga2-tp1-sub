bb21629c300c3febb7fc8682902fd05dce9ce29e
jluini <jluini@f1292de8-a922-11de-9ed3-3f1bf8abe145>
2009-11-18 14:52:34 +0000

    Anda freichen en X y mejoro simple.c

--- tp_2/src/simd/simple.c
+++ tp_2/src/simd/simple.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 
-struct IplImage {
-    int width;
-    int height;
-    char* imageData;
-};
-
-
 /**
  * Reemplazar por otros operadores...
  */
-extern void simdPrewitt(const char* src, char* dst, int ancho, int alto);
+extern void simdRoberts(const char* src, char* dst, int ancho, int alto);
+
+
+struct IplImage {
+    int width;
+    int height;
+    unsigned char* imageData;
+};
 
 
 /* Funciones de imagenes */
@@ -22,82 +22,76 @@ extern void simdPrewitt(const char* src, char* dst, int ancho, int alto);
  * resultado en 'dst'. Asume que son de igual tamaño.
  */
 void aplicar3x3(struct IplImage src, struct IplImage dst, float matriz[3][3]);
+void cRoberts(struct IplImage src, struct IplImage dst);
 
 /**
  * Calcula el operador en un pixel.
  */
-char calcular(struct IplImage img, int i, int j, float matriz[3][3]);
+unsigned char calcular(struct IplImage img, int i, int j, float matriz[3][3]);
 
 /**
  * Devuelve un pixel del dibujo casteado a float.
  */
 float get(const struct IplImage img, int i, int j);
+/**
+ * Cambia un pixel del dibujo.
+ */
+float set(const struct IplImage img, int i, int j, unsigned char value);
+
+/**
+ * Crean una imagen inicialmente en negro.
+ */
+struct IplImage* newIplImage(int width, int height); 
+
+void freeIplImage(struct IplImage* img);
+
+void clear(struct IplImage img);
+void llenar1(struct IplImage img);
+void llenar2(struct IplImage img);
+void llenarRandom(struct IplImage img);
 
 
+unsigned char nextRnd();
+
+ 
+/** 
+ * Muestra por salida estándar los datos de una IplImage.
+ */
 void printData(struct IplImage* img);
 
+/**
+ * Muestra por salida estándar los datos de una IplImage.
+ */
+void printDataCompacta(struct IplImage* img);
 
 
 int main(int argc, char *argv[]){
-
-
-    int width = 80;     // width debe ser múltiplo de 16
-    int height = 40;
+    int width = 32;     // width debe ser múltiplo de 16
+    int height = 10;
         
-    char* srcData = malloc(width*height);        //Reservo width*height bytes para los datos
-    char* dstData = malloc(width*height);
-
     int i, j;
     
-    for(i = 0; i < height; i += 1) {
-        for(j = 0; j < width; j += 1) {
-            srcData[i * width + j] = (j + i + 48) % 256;
-            dstData[i * width + j] = 0;
-        }
-    }
+    struct IplImage* src = newIplImage(width, height);
+    llenarRandom(*src);
     
-    struct IplImage* src; src = malloc(9);
-    src->width = width; src->height = height; src->imageData = srcData;
-
-    struct IplImage* dst; dst = malloc(9);
-    dst->width = width; dst->height = height; dst->imageData = dstData;
-    
-    //printf("len(srcData)=%d  len(dstData)=%d \n", strlen(srcData), strlen(dstData));
-    //printf("%s, %d, %d\n", src->imageData, src->width, src->height);
-    //printf("%s, %d, %d\n", dst->imageData, dst->width, dst->height);
-
-    //printf("srcData=%d dstData=%d \n", (int)srcData, (int)dstData);
-    //printf("srcData[0:1] = %d:%d\n", srcData[0], srcData[1]);
-    //printf("srcData[15:17] = %d:%d:%d\n", srcData[15], srcData[16], srcData[17]);
-
-    printf("Antes:\n");
     printf("  src=");
     printData(src);
     printf("\n");
-    printf("  dst=");
-    printData(dst);
-    printf("\n");
     
-    //asmPrewittSIMD(src->imageData, dst->imageData, src->width, src->height);
-
+    struct IplImage* dst = newIplImage(width, height);
     simdPrewitt(src->imageData, dst->imageData, src->width, src->height);
     
-    //cRoberts(src->imageData, dst->imageData, src->width, src->height, 1, 0);
-
-    
-    printf("Dsps:\n");
-    printf("  src=");
-    printData(src);
-    printf("\n");
     printf("  dst=");
     printData(dst);
     printf("\n");
     
-    
-    return 0;
+    freeIplImage(src);
+    freeIplImage(dst);
+        
 
-	
+    return 0;
 }
+
 
 void printData(struct IplImage* img) {
     int i, j;
@@ -105,8 +99,28 @@ void printData(struct IplImage* img) {
         printf("\n      ");
         for(j = 0; j < img->width; j++) {
             if(j > 0) printf(",");
-            if(j == 8) printf("  ");
-            printf("%d", img->imageData[i * img->width + j]);
+            unsigned char val = get(*img, i, j);
+            
+            if(val < 100) printf(" ");
+            if(val < 10) printf(" ");
+            
+            printf("%d", val);
+        }
+    }
+}
+
+void printDataCompacta(struct IplImage* img) {
+    int i, j;
+    for(i = 0; i < img->height; i++) {
+        printf("\n");
+        for(j = 0; j < img->width; j++) {
+            //if(j > 0) printf(",");
+            unsigned char val = get(*img, i, j) / 10;
+            
+            //if(val < 100) printf(" ");
+            if(val < 10) printf(" ");
+            
+            printf("%d", val);
         }
     }
 }
@@ -149,56 +163,140 @@ void addps(float* dest, float* src) {
 /* Funciones de imagen */
 
 float get(const struct IplImage img, int i, int j) {
-	return (float) img.imageData[i * img.width + j];
+    return (float) img.imageData[i * img.width + j];
 }
 
+float set(const struct IplImage img, int i, int j, unsigned char value) {
+    img.imageData[i * img.width + j] = value;
+}
 
-char calcular(struct IplImage img, int i, int j, float matriz[3][3]) {
-	float dx;
-	dx = matriz[0][0] * get(img, i - 1, j - 1)
-	   + matriz[0][1] * get(img, i - 1, j    )
-	   + matriz[0][2] * get(img, i - 1, j + 1)
-	   + matriz[1][0] * get(img, i    , j - 1)
-	   + matriz[1][1] * get(img, i    , j    )
-	   + matriz[1][2] * get(img, i    , j + 1)
-	   + matriz[2][0] * get(img, i + 1, j - 1)
-	   + matriz[2][1] * get(img, i + 1, j    )
-	   + matriz[2][2] * get(img, i + 1, j + 1);
-	   
-	if(dx <= 0) {
-		return (char)0;
-	} else if(dx >= 255) {
-		return (char)255;
-	} else {
-		return (char)dx;	
-	}
+unsigned char calcular(struct IplImage img, int i, int j, float matriz[3][3]) {
+    float dx;
+    dx = matriz[0][0] * get(img, i - 1, j - 1)
+       + matriz[0][1] * get(img, i - 1, j    )
+       + matriz[0][2] * get(img, i - 1, j + 1)
+       + matriz[1][0] * get(img, i    , j - 1)
+       + matriz[1][1] * get(img, i    , j    )
+       + matriz[1][2] * get(img, i    , j + 1)
+       + matriz[2][0] * get(img, i + 1, j - 1)
+       + matriz[2][1] * get(img, i + 1, j    )
+       + matriz[2][2] * get(img, i + 1, j + 1);
+       
+    if(dx <= 0) {
+        return (unsigned char)0;
+    } else if(dx >= 255) {
+        return (unsigned char)255;
+    } else {
+        return (unsigned char)dx;    
+    }
 }
 
 
 void aplicar3x3(struct IplImage src, struct IplImage dst, float matriz[3][3]) {
-	int i, j;
-	int sum;
-	char px;
-	char res;
-	
-	for(i = 1; i < src.height - 1; i++) {
-		for(j = 1; j < dst.width - 1; j++) {
-			px = calcular(src, i, j, matriz);
-			
-			sum = (int)px;
-			sum += (int)get(dst, i, j);
+    int i, j;
+    int sum;
+    unsigned char px;
+    unsigned char res;
+    
+    for(i = 1; i < src.height - 1; i++) {
+        for(j = 1; j < dst.width - 1; j++) {
+            px = calcular(src, i, j, matriz);
+            
+            sum = (int)px;
+            sum += (int)get(dst, i, j);
 
-			if(sum <= 0) {
-				res = (char)0;
-			} else if(sum >= 255) {
-				res = (char)255;
-			} else {
-				res = (char)sum;
-			}
-			
-			dst.imageData[i * src.width + j] = res;	
-		}
-	}
+            if(sum <= 0) {
+                res = (unsigned char)0;
+            } else if(sum >= 255) {
+                res = (unsigned char)255;
+            } else {
+                res = (unsigned char)sum;
+            }
+            
+            set(dst, i, j, res);
+        }
+    }
 }
 
+struct IplImage* newIplImage(int width, int height) {
+    unsigned char* data = malloc(width*height);        //Reservo width*height bytes para los datos
+    
+    int i, j;
+    
+    for(i = 0; i < height; i += 1) {
+        for(j = 0; j < width; j += 1) {
+            data[i * width + j] = (unsigned char)0;
+        }
+    }
+    
+    struct IplImage* res = malloc(9);
+    res->width = width; res->height = height; res->imageData = data;
+    
+    return res;
+}
+
+void freeIplImage(struct IplImage* img) {
+    free(img->imageData);
+    free(img);    
+}
+
+void clear(struct IplImage img) {
+    int i, j;
+    for(i = 0; i < img.height; i++) {
+        for(j = 0; j < img.width; j++) {
+            set(img, i, j, (unsigned char)0);
+        }
+    }
+}
+
+void llenar1(struct IplImage img) {
+    int i, j;
+    for(i = 0; i < img.height; i++) {
+        for(j = 0; j < img.width; j++) {
+            set(img, i, j, (unsigned char)((j + i) % 256));
+        }
+    }
+}
+void llenar2(struct IplImage img) {
+    int i, j;
+    for(i = 0; i < img.height; i++) {
+        for(j = 0; j < img.width; j++) {
+            set(img, i, j, (unsigned char)(((j + i) % 7) * 111));
+        }
+    }
+}
+
+void llenarRandom(struct IplImage img) {
+    int i, j;
+    for(i = 0; i < img.height; i++) {
+        for(j = 0; j < img.width; j++) {
+            set(img, i, j, nextRnd());
+        }
+    }
+}
+
+int rndA = 1010; int rndB = 17;
+int rndC = 234;
+unsigned char nextRnd() {
+    rndA = rndA * 7 + 11;
+    rndB = rndB * 11 + 13;
+    rndC = rndC * 13 + 17;
+    
+    
+    return (rndA+rndB+rndC) % 256;
+}
+
+void cRoberts(struct IplImage src, struct IplImage dst) {
+    
+    float xMatriz[3][3] = {{ 0, 0, 0},
+                           { 0, 1, 0},
+                           { 0, 0,-1}};
+    float yMatriz[3][3] = {{ 0, 0, 0},
+                           { 0, 0, 1},
+                           { 0,-1, 0}};
+    
+    aplicar3x3(src, dst, xMatriz);
+    aplicar3x3(src, dst, yMatriz);
+
+}
 
