122c61cefa18985310d155404d3b45a3128d5174
nicog89 <nicog89@f1292de8-a922-11de-9ed3-3f1bf8abe145>
2009-11-22 19:08:57 +0000

    Puse todos las interrupciones, y creo que termine el ejercicio 4! El martes ya lo terminamos y hacemos el infomre q tamos al horno sino.

--- TP3/src/isr.h
+++ TP3/src/isr.h
@@ -6,16 +6,25 @@
  * TODO: Agregar el resto de las ISR
  */
 void _isr0(); //DIVIDE ERROR
+void _isr1(); //RESERVED
+void _isr2(); //NMI INTERRUPT
+void _isr3(); //BREAKPOINT
 void _isr4(); //OVERFLOW
+void _isr5(); //BOUND RANGE EXCEEDED
 void _isr6(); //INVALID OPCODE
 void _isr7(); //DEVICE NOT AVAILABLE
 void _isr8(); //DOUBLE FAULT
+void _isr9(); //COPROCESSOR SEGMENT OVERRUN
 void _isr10();//INVALID TSS
 void _isr11();//SEGMENT NOT PRESENT
 void _isr12();//STACK-SEGMENT FAULT
 void _isr13();//GENERAL PROTECTION
 void _isr14();//PAGE FAULT
+void _isr15();//INTEL RESERVERD
 void _isr16();//FPU ERROR
+void _isr17();//ALIGNMENT CHEHCK
+void _isr18();//MACHINE CHECHK
+void _isr19();//SIMD FLOATING-POINT
 void _isr32(); //RELOJ
 //void _isr33();//TECLADO
 /* **************************************************************
